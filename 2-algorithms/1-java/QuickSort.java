ebe18a8eae7fb363c749328f9e3230fc87f4e5e3
Chethan Pai Maroor <121536127+CheetahCodes21@users.noreply.github.com>
2023-10-16 18:35:20 +0530

    quick sort - java

---
+++ 2-algorithms/1-java/QuickSort.java
@@ -0,0 +1,43 @@
+public class QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swap arr[i+1] and arr[high] (put pivot element in its correct position)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 3};
+
+        System.out.println("Original array: " + Arrays.toString(arr));
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
